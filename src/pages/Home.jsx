f39023504bcb72d2c98faad4cfa1a6a861de01d1
takumi12311123 <takumiakasaka1231@gmail.com>
2024-10-05 23:14:14 +0900

    fix: change path for product

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   const navigate = useNavigate();
 
   const handleClickAbout = () => {
-    navigate("/about");
+    navigate("/product");
   };
 
   const handleClickNews = () => {
