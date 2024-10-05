cff54ddf239488c09b6f0bdccf68913cb15e2a47
takumi12311123 <takumiakasaka1231@gmail.com>
2024-10-05 23:02:07 +0900

    feature: add each page template

---
+++ src/pages/News2.jsx
@@ -0,0 +1,11 @@
+// こちらは、/news/2 のページです
+
+import { Button } from "../components/Button";
+import { Image } from "../components/Image";
+import { useNavigate } from "react-router-dom";
+
+export default function News2() {
+  const navigate = useNavigate();
+
+  return <div></div>;
+}
