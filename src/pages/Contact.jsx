3161420b9b402835ac249383ca90f3f45ee7d2b8
Sato <167418137+yatai310@users.noreply.github.com>
2024-10-20 02:23:34 +0000

    お問い合わせ基礎

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,23 @@
 // こちらは、/contact 会社へのお問い合わせページです。
 
-// import { Button } from "../components/Button";
-// import { Image } from "../components/Image";
-// import { useNavigate } from "react-router-dom";
+import { Button } from "../components/Button";
+import { Image } from "../components/Image";
+import { useNavigate } from "react-router-dom";
 
 export default function Contact() {
-  // const navigate = useNavigate();
-
-  return <div></div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>お問い合わせ</h1>
+      <form action="" method="get">
+        <div>
+          <label for="name">Enter your name: </label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div>
+          <input type="submit" value="Subscribe!" />
+        </div>
+      </form>
+    </div>
+  );
 }
