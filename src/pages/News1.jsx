3220d9b780fe325f5adc7423de838a5aa86e4cde
akane <s1310192@u-aizu.ac.jp>
2024-10-20 04:57:26 +0000

    news作成

--- src/pages/News1.jsx
+++ src/pages/News1.jsx
@@ -1,11 +1,37 @@
 // こちらは、/news/1 のページです
 
-// import { Button } from "../components/Button";
-// import { Image } from "../components/Image";
-// import { useNavigate } from "react-router-dom";
+import { Button } from "../components/Button";
+import { Image } from "../components/Image";
+import { useNavigate } from "react-router-dom";
 
-export default function News1() {
-  // const navigate = useNavigate();
+export default function News() {
+  const navigate = useNavigate();
 
-  return <div></div>;
+  return (
+    <div>
+      <a href="/news" className="hover:underline">
+        一覧へ戻る
+      </a>
+      <br />
+
+      <br />
+      <h2>PM体験とプログラミング開発を学ぶ2daysが開催されました!</h2>
+      <br />
+      <Image
+        src="https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqG4_7QpR89DnbhG4YLv-247oP-exZ-24UX98A_XultMjYf1aomSn04JRUAiCk6xkoxW3KXzGko9lg8TfgvmOBzloo87I8iWcPkN_XkWGPX8c_aE4-W9uPEB76D8ZD-3inzFlfkK3o4dD7k4In05KtsC_LiJlOV258tTKjNtwKxI1RQ3DglMTuTp3wPNoJ7b3rMB5pR2CwhL23wmJWuSxEnNfVzmyM8XjjSgvFrvhP7I4O/DSC01009-Pano-scaled.jpg?errorImage=false"
+        alt="aizubase"
+        style={{ maxWidth: "100%", height: "auto" }}
+      />
+      <br />
+      <p>
+        2024年10月19日・20日の2日間、会津大生を対象としたイベントがAIZU-BASEにて開催されました。
+        このイベントでは、13名の学生が集まり、PM体験をプログラミング開発を学びました。
+        初日は企画説明と基礎講座、2日目は3,4人のグループを作り企業ホームページの作成を疑似体験しました。
+        どのグループも個性のあるホームページを作成することができました!
+      </p>
+      <a href="/news/2" className="hover:underline">
+        next→
+      </a>
+    </div>
+  );
 }
--- src/pages/News2.jsx
+++ src/pages/News2.jsx
@@ -1,11 +1,39 @@
 // こちらは、/news/2 のページです
 
-// import { Button } from "../components/Button";
-// import { Image } from "../components/Image";
-// import { useNavigate } from "react-router-dom";
+import { Button } from "../components/Button";
+import { Image } from "../components/Image";
+import { useNavigate } from "react-router-dom";
 
-export default function News2() {
-  // const navigate = useNavigate();
+export default function News1() {
+  const navigate = useNavigate();
 
-  return <div></div>;
+  return (
+    <div>
+      <a href="/news" className="hover:underline">
+        一覧へ戻る
+      </a>
+      <br />
+      <a href="/news/1" className="hover:underline">
+        ←prev
+      </a>
+      <br />
+      <br />
+      <h2>「卒論テーマの公開」と「学生課によるガイダンス」について</h2>
+      <br />
+      <Image
+        src="https://u-aizu.ac.jp/files/images/2019finallecture11.JPG"
+        alt="daikogi"
+        style={{ maxWidth: "100%", height: "auto" }}
+      />
+      <br />
+      <p>
+        2024年10月18日、会津大学の学部2年生を対象としたガイダンスが催されました。
+        会津大学では、多くの場合2年生の後期に配属する研究室とフィールド、卒業論文のテーマを決め、3年生から配属されます。
+        みんな真面目に話を聞いていました。
+      </p>
+      <a href="/news/3" className="hover:underline">
+        next→
+      </a>
+    </div>
+  );
 }
