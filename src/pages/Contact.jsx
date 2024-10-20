99b0cc7d47e775cf3f1b279de5df02ba24ab46eb
yururi <166689675+yurunull@users.noreply.github.com>
2024-10-20 02:46:51 +0000

    add color

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,45 @@
-// こちらは、/contact 会社へのお問い合わせページです。
+//こちらは、/contact 会社へのお問い合わせページです。
 
-// import { Button } from "../components/Button";
-// import { Image } from "../components/Image";
-// import { useNavigate } from "react-router-dom";
+import { Button } from "../components/Button";
+import { Image } from "../components/Image";
+import { useNavigate } from "react-router-dom";
 
 export default function Contact() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  return <div></div>;
+  return (
+    <form action="" method="get" className="max-w-wd mx-auto mt-8">
+      <div className="text-saddlebrow">
+        <label htmlFor="name">Enter your name: </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="border border-indigo-600"
+        />
+      </div>
+      <div className=".py-2">
+        <label htmlFor="comment">Enter your comment: </label>
+        <input
+          type="text"
+          name="comment"
+          id="comment"
+          className="border border-indigo-600"
+        />
+      </div>
+      <div className="text-saddlebrow">
+        <input
+          type="submit"
+          value="Submit"
+          className="border border-indigo-600"
+        />
+      </div>
+    </form>
+    // <input
+    //   className="border border-indigo-600"
+    //   type="text"
+    //   name="name"
+    //   id="name"
+    // />
+  );
 }
