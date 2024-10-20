69e5254475e2a16ae70aa718c407c7fd233b5193
seg <segnities007@gmail.com>
2024-10-20 12:28:54 +0900

    create recruitment ui except deep ui and function

--- src/pages/Recruitment.jsx
+++ src/pages/Recruitment.jsx
@@ -1,4 +1,5 @@
 // こちらは、/recruitment 採用活動ページです
+// 参考ページ: https://herp.careers/v1/wedinc
 
 // import { Button } from "../components/Button";
 // import { Image } from "../components/Image";
@@ -7,5 +8,36 @@
 export default function Recruitment() {
   // const navigate = useNavigate();
 
-  return <div></div>;
+  return <div>
+    <h1 className="p-10">
+      WED株式会社(偽) のすべての求人一覧
+    </h1>
+    <RecruitItem title="title" subtitle="subtitle" description="description" />
+    <RecruitItem title="title" subtitle="subtitle" description="description" />
+    <RecruitItem title="title" subtitle="subtitle" description="description" />
+    <RecruitItem title="title" subtitle="subtitle" description="description" />
+    <RecruitItem title="title" subtitle="subtitle" description="description" />
+  </div>;
+}
+
+function　RecruitItem({
+  title,
+  subtitle,
+  description,
+}){
+  return(
+    <div className="px-4 py-4">
+      <div className="bg-card text-card-foreground p-4 rounded-lg shadow-md">
+        <p className="border-l-4 border-blue-500 pl-4">
+          {title}
+        </p>
+        <p>
+          {subtitle}
+        </p>
+        <p>
+          {description}
+        </p>
+      </div>
+    </div>
+  );
 }
